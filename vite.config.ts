c4e3c19169c0397e4120413cfda2d13d0d550199
Nicholas <111785905+nick-thomp@users.noreply.github.com>
2025-12-27 21:40:43 -0600

    Update icon and serviceworker

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,30 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    VitePWA({ registerType: 'autoUpdate'})
+    VitePWA({ 
+      registerType: 'prompt',
+      injectRegister: 'auto',
+      manifest: {
+        name: 'Book Tracker',
+        short_name: 'BT',
+        description: 'A simple app to help track books',
+        theme_color: '#528593',
+        icons: [
+          {
+            src: '192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*, {js,css,html,ico,png,svg}']
+      }
+    })
   ],
 })
